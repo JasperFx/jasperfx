ab01bd75edef28b0f0fdf9ed379731ace102c256
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-10 11:40:30 -0600

    Moved WalkReferencedAssemblies to JasperFx

---
+++ src/JasperFx/Core/Reflection/WalkReferencedAssemblies.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace JasperFx.Core.Reflection;
+
+public static class WalkReferencedAssemblies
+{
+    public static IEnumerable<Assembly> ForTypes(params Type[] types)
+    {
+        var stack = new Stack<Type>();
+
+        foreach (var type in types)
+        {
+            stack.Push(type);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                yield return current.Assembly;
+
+                if (!current.IsGenericType || current.IsGenericTypeDefinition)
+                {
+                    continue;
+                }
+
+                var typeArguments = current.GetGenericArguments();
+                foreach (var typeArgument in typeArguments) stack.Push(typeArgument);
+            }
+        }
+    }
+}
