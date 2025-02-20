02c782886fd34f42e8b817ee019d67e62d904662
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-02-20 11:48:29 -0600

    Moved StreamIdentity to JasperFx.Events

---
+++ src/JasperFx.Events/StreamIdentity.cs
@@ -0,0 +1,18 @@
+#nullable enable
+namespace JasperFx.Events;
+
+/// <summary>
+///     Specify the identity strategy for event streams
+/// </summary>
+public enum StreamIdentity
+{
+    /// <summary>
+    ///     Streams should be identified by Guid
+    /// </summary>
+    AsGuid,
+
+    /// <summary>
+    ///     Streams should be identified by a user supplied string
+    /// </summary>
+    AsString
+}
