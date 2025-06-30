9ba605ab0c4e9901b6892ae9dc55cbae90dbbf3d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-06-30 11:48:32 -0500

    Added Version to EventStoreUsage. JasperFx.Events 1.3

--- src/JasperFx.Events/Descriptors/EventStoreUsage.cs
+++ src/JasperFx.Events/Descriptors/EventStoreUsage.cs
@@ -11,8 +11,10 @@ public class EventStoreUsage : OptionsDescription
     public EventStoreUsage(Uri subjectUri, object subject) : base(subject)
     {
         SubjectUri = subjectUri;
+        Version = subject.GetType().Assembly.GetName().Version;
     }
-
+    
+    public Version Version { get; set; }
     public Uri SubjectUri { get; set; }
     public DatabaseUsage Database { get; set; }
     public List<EventDescriptor> Events { get; set; } = new();
