ba2e47ac3127bf937e7a3f19cd49a9255e0a2b8f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-07-24 16:36:25 -0500

    Moved EventIdentity to its own file

--- src/JasperFx.Events/Event.cs
+++ src/JasperFx.Events/Event.cs
@@ -1,33 +1,8 @@
 using System.Linq.Expressions;
 using FastExpressionCompiler;
-using ImTools;
 using JasperFx.Core.Reflection;
 
 namespace JasperFx.Events;
-
-public static class EventIdentity
-{
-    private static ImHashMap<Type, object> _functions = ImHashMap<Type, object>.Empty;
-    
-    /// <summary>
-    /// Use to get an identity off of an IEvent for single stream aggregations
-    /// </summary>
-    /// <param name="e"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static T IdentityFor<T>(IEvent e) where T : notnull
-    {
-        if (_functions.TryFind(typeof(T), out var raw))
-        {
-            if (raw is Func<IEvent, T> func) return func(e);
-        }
-
-        var identitySource = IEvent.CreateAggregateIdentitySource<T>();
-        _functions = _functions.AddOrUpdate(typeof(T), identitySource);
-
-        return identitySource(e);
-    }
-}
 
 #region sample_IEvent
 
